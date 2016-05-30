bdba8c1b91dbb4ea539836e688082ab80de8cb54
Scott Davidson <scott@scott-davidson.com>
2016-05-30 14:20:25 -0700

    Added TestRenderMain to show how to render a partial grid

---
+++ src/com/davidsoncamp/tenten/util/TestRenderMain.java
@@ -0,0 +1,19 @@
+package com.davidsoncamp.tenten.util;
+
+public class TestRenderMain {
+
+	public static void main(String[] args) {
+		System.out.println("||----||----||");
+		System.out.println("|| 0  || 0  ||");
+		System.out.println("||  0 ||  1 ||");
+		System.out.println("||----||----||");
+		System.out.println("+++++++++++++++++");
+		System.out.println("||----||----||");
+		System.out.println("|| ** || ** ||");
+		System.out.println("|| ** || ** ||");
+		System.out.println("||----||----||");
+		System.out.println("|| ** || 0  ||");
+		System.out.println("|| ** ||  1 ||");
+		System.out.println("||----||----||");
+	}
+}
